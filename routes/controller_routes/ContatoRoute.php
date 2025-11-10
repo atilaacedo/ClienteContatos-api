aa627c0983ce7227c15e21a039a2bcbb067adb69
atilaacedo <atilamacedo001@gmail.com>
2025-11-10 14:47:33 -0300

    fix: fix routes for add new emails or telefones

--- routes/controller_routes/ContatoRoute.php
+++ routes/controller_routes/ContatoRoute.php
@@ -1,6 +1,12 @@
 <?php
 
 use App\Http\Controllers\ContatoController;
+use App\Http\Controllers\ContatoTelefoneController;
 use Illuminate\Support\Facades\Route;
 
 Route::apiResource('contatos', ContatoController::class);
+
+Route::prefix('contatos/{contato}')->group(function () {
+    Route::post('telefones', [ContatoTelefoneController::class, 'store']);
+    ROute::post('emails', [ContatoTelefoneController::class, 'storeEmail']);
+});
